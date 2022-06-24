af024987a5e04ade652b302c81dfcb58f626da55
Alessandro Astone <ales.astone@gmail.com>
2022-06-24 08:27:33 +0000

    sdm439-common: Build & Enable fastbootd
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -25,6 +25,10 @@ OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+    
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
